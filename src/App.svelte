eecbfa5833cf25a44c10347a0540e53849740632
rai <mj82xx@gmail.com>
2023-08-18 11:14:33 +0900

    step1: コンポーネントを作ってみよう

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+  import TodoItem from "./lib/TodoItem.svelte";
 </script>
 
 <main>
-  <h1>Hello, Svelte!</h1>
+  <h2>ToDo List</h2>
+  <div>
+    <TodoItem id={1} title="Test1" />
+    <TodoItem id={2} title="Test2" />
+  </div>
+  <form>
+    <input type="text" placeholder="Add a new ToDo" />
+    <button>Add</button>
+  </form>
 </main>
 
 <style>
+  form {
+    margin-top: 1rem;
+  }
 </style>
---
+++ src/lib/TodoItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let id: number;
+  export let title: string;
+</script>
+
+<div>
+  <input type="checkbox" id={id.toString()} />
+  <label for={id.toString()}>{title}</label>
+</div>
